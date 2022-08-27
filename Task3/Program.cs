5c2370ef2a6af6211341c797a821fe454a577ac6
Tati <tanushka-p.94@mail.ru>
2022-08-27 16:42:53 +0300

    Save 1 Task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Подсчитать сумму цифр в числе
+int A = new Random().Next(1,1000);
+Console.WriteLine(A);
+int x = A;
+int i = 0;
+int sum = 0;
+while (x > 0 )
+{
+    int digit = x % 10;
+    sum = sum + digit;
+    x = x/10;
+}
+Console.WriteLine($"Сумма цифр в числе  равна : {sum}");
