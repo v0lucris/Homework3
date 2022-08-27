43270d5b343c00ac028dbefd3eb69300b65b0207
Tati <tanushka-p.94@mail.ru>
2022-08-27 16:55:46 +0300

    save1 task4

---
+++ Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.WriteLine("Введи число N : ");
+int N = int.Parse(Console.ReadLine());
+
+int i = 1;
+int P = 1;
+while (i <= N)
+{
+    P = P * i;
+        i++;
+}
+
+Console.WriteLine($"Произведение чисел от 1 до {N} равно : {P}" );
