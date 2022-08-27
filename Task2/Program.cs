3db34ed959a3eb992484db7d21bd82b168b804b6
Tati <tanushka-p.94@mail.ru>
2022-08-27 15:52:36 +0300

    save 1 task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+Console.WriteLine("Введи число A : ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введи степень B : ");
+int B = int.Parse(Console.ReadLine());
+int P = 1;
+int i = 1;
+while(i <= B)
+{
+    P = P * A;
+    i++;
+}
+Console.WriteLine($"число А в натуральной степени B равно : {P} ");
