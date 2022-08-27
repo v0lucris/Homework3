1e8234a4ad32b52580b4e3319635239882883b0b
Tati <tanushka-p.94@mail.ru>
2022-08-27 17:53:33 +0300

    save1 task5

---
+++ Task5/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.WriteLine("Введи число N : ");
+int N = int.Parse(Console.ReadLine());
+
+int i = 1;
+while (i <= N )
+{
+ int P = i * i * i;
+    if (P % 2 == 0)
+    {
+        Console.WriteLine($"Куб числа {i} равен : {P}");
+    }
+i++;
+}
+
+
